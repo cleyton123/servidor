5d08bc9c93177825295cad7462b5c57f58817628
cleyton <cleyton.p2021@gmail.com>
2024-04-28 15:23:12 -0300

    noovo

--- scr/config/dbConnect.js
+++ scr/config/dbConnect.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 async function connectDatabase(){
-  const password = "cp456789"; 
-  mongoose.connect(`mongodb+srv://cleyton:${password}@cp456789.mongodb.net/MongoDB?retryWrites=true&w=majority&appName=MongoDB`);
+  mongoose.connect('mongodb+srv://cleyton:cp456789@mongodb.dm2mq6p.mongodb.net/?retryWrites=true&w=majority&appName=MongoDB ');
   
   return mongoose.connection;
 }
